28190dc1537c87485bb7584eecbc8ee6ecfe674e
edelgado-git <enriquedelgado806@gmail.com>
2023-05-21 10:26:15 -0400

    Add image picker

--- app/screens/NewJobScreen.js
+++ app/screens/NewJobScreen.js
@@ -6,15 +6,34 @@ import AppForm from '../components/AppForm'
 import AppFormField from '../components/AppFormField'
 import SubmitButton from '../components/SubmitButton'
 import Screen from '../components/Screen'
+import FormImagePicker from '../components/FormImagePicker'
 
 const NewJobScreen = () => {
   return (
-    <Screen>
-        <View>
-            <Text>New Job</Text>
-        </View>
+    <Screen style={styles.container}>
+        <AppForm
+            initialValues={{
+                tailNumber: "",
+                customer: null,
+                aircraftType: null,
+                airport: null,
+                fbo: null,
+                comment: "",
+                images: []
+            }}
+            onSubmit={(values) => console.log(values)}
+            >
+            <FormImagePicker name="images" />
+
+        </AppForm>
     </Screen>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    }
+})
 
 export default NewJobScreen
